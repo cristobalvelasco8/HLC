f4604b3df9e9c382ae6e208cc7fb88767a77c48e
CRISTOBAL VELASCO VILCHEZ <71633086+cristobalvelasco8@users.noreply.github.com>
2023-03-10 17:40:55 +0100

    ADD

---
+++ mavenproject1/target/generated-sources/annotations/model/Cliente_.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-02-14T18:02:08")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, String> apellidos;
+    public static volatile SingularAttribute<Cliente, Date> factualizar;
+    public static volatile SingularAttribute<Cliente, String> direccion;
+    public static volatile SingularAttribute<Cliente, Date> fregistro;
+    public static volatile SingularAttribute<Cliente, Long> id;
+    public static volatile SingularAttribute<Cliente, String> telefono;
+    public static volatile SingularAttribute<Cliente, String> email;
+    public static volatile SingularAttribute<Cliente, String> nombres;
+
+}
